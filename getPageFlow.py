64cc3b0c1bb9647fa27f02cea63f19857a50895e
guava <gguava@gmail.com>
2012-07-04 00:27:32 +0800

    fix the gbk code
    x2=buf.decode('gbk','ignore').encode( 'utf-8' )

---
+++ getPageFlow.py
@@ -0,0 +1,37 @@
+#_*_encoding:utf-8_*_
+
+import  urllib
+
+class GetOnePageFlow:
+	url=""
+	content=""
+	def openNewthread(self):
+		print "ok"
+		
+	def setUrl(self, url):
+		self.url=url
+		print self.url
+		
+	def getpage(self):
+		f = urllib.urlopen(self.url)
+		print f.getcode()
+		#print f.info()
+		buf=f.read()
+		#print buf.encode( 'utf-8' )
+		
+		f = open('spath.html', "w" ) 
+		f.write( buf)
+		
+		x2=buf.decode('gbk','ignore').encode( 'utf-8' )
+		f = open('spath2.html', "w" ) 
+		f.write( x2)
+		
+	
+	def save2db(self):
+		print "ok"
+		
+	
+x=GetOnePageFlow()
+x.setUrl("http://www.biquge.com/0_200/469850.html")
+
+x.getpage()
